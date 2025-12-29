189eed4de2f5a4a5444ec2e4526afcc89451dccb
FranBec <franbecvort@gmail.com>
2025-12-29 15:20:36 +0000

    initial commit

---
+++ spring_groovy/src/main/groovy/dev/pollito/spring_groovy/SpringGroovyApplication.groovy
@@ -0,0 +1,13 @@
+package dev.pollito.spring_groovy
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringGroovyApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpringGroovyApplication, args)
+	}
+
+}
---
+++ spring_java/src/test/java/dev/pollito/spring_java/SpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pollito.spring_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
